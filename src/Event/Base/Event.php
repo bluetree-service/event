e2fe798fa5714f6815da9c2e9606e23eb853c561
chajr <chajr@bluetree.pl>
2015-05-28 17:54:30 +0200

    Finish Event logic (issue #3)
        Details of additions/deletions below:
        --------------------------------------------------------------
    
        Modified:   Event/Base/Event.php
                    -- Added --
                       $_launchCount store information how many times event was called
                       $_propagationStopped store information that event propagation is stopped or not
                       __construct
                       getLaunchCount return number how many times event was called
                    -- Updated --
                        isPropagationStopped added method logic
                        stopPropagation added method logic
    
        Modified:   Event/Base/Interfaces/EventInterface.php
                    -- Added --
                        __construct
                        getLaunchCount

--- src/Event/Base/Event.php
+++ src/Event/Base/Event.php
@@ -1,10 +1,8 @@
 <?php
 /**
  * @todo tags
- * @todo launch count
  * @todo last trigger
  * @todo tracer
- * @todo stop propagation
  * @todo log event
  */
 
@@ -14,13 +12,56 @@ use ClassEvent\Event\Base\Interfaces\EventInterface;
 
 abstract class Event implements EventInterface
 {
-    public function isPropagationStopped()
+    /**
+     * store information how many times event was called
+     *
+     * @var int
+     */
+    protected static $_launchCount = 0;
+
+    /**
+     * store information that event propagation is stopped or not
+     *
+     * @var bool
+     */
+    protected $_propagationStopped = false;
+
+    /**
+     * create event instance
+     */
+    public function __construct()
     {
-        
+        self::$_launchCount++;
     }
 
+    /**
+     * return number how many times event was called
+     *
+     * @return int
+     */
+    public static function getLaunchCount()
+    {
+        return self::$_launchCount;
+    }
+
+    /**
+     * return information that event propagation is stopped or not
+     *
+     * @return bool
+     */
+    public function isPropagationStopped()
+    {
+        return $this->_propagationStopped;
+    }
+
+    /**
+     * allow to stop event propagation
+     *
+     * @return $this
+     */
     public function stopPropagation()
     {
-
+        $this->_propagationStopped = true;
+        return $this;
     }
 }
--- src/Event/Base/Interfaces/EventInterface.php
+++ src/Event/Base/Interfaces/EventInterface.php
@@ -4,6 +4,8 @@ namespace ClassEvent\Event\Base\Interfaces;
 
 interface EventInterface
 {
+    public function __construct();
+    public static function getLaunchCount();
     public function isPropagationStopped();
     public function stopPropagation();
 }
